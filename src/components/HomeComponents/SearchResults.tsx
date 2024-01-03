9d1fdfc0a2e5242307aebab9187e1ddd58d12c09
darshan <darshpat01@gmail.com>
2024-01-03 23:58:21 +0530

    ui - updated trend images

--- src/components/HomeComponents/SearchResults.tsx
+++ src/components/HomeComponents/SearchResults.tsx
@@ -1,5 +1,9 @@
 import TrendItem from "./TrendItem";
 import img1 from "../../images/img1.jpeg";
+import img2 from "../../images/img2.jpeg";
+import img3 from "../../images/img3.jpeg";
+import img4 from "../../images/img4.jpeg";
+import img5 from "../../images/img5.jpeg";
 const divStyle =
   "my-2 hover:cursor-pointer hover:font-bold ease-in duration-100";
 const SearchResults: React.FC = () => (
@@ -8,10 +12,10 @@ const SearchResults: React.FC = () => (
       <div className="font-semibold text-xl mb-3">Latest Trends</div>
       <div className="flex flex-wrap  justify-between">
         <TrendItem img={img1} text="Shirt with puffed sleeves" />
-        <TrendItem img={img1} text="Linen jumpsuit" />
-        <TrendItem img={img1} text="White formal suit" />
-        <TrendItem img={img1} text="Pattern dresses" />
-        <TrendItem img={img1} text="Leather shirt dress" />
+        <TrendItem img={img2} text="Linen jumpsuit" />
+        <TrendItem img={img3} text="White formal suit" />
+        <TrendItem img={img4} text="Pattern dresses" />
+        <TrendItem img={img5} text="Leather shirt dress" />
       </div>
       <div className="font-semibold text-xl mt-6 mb-3">Popular suggestions</div>
       <div className={divStyle}>Striped shirt dress</div>
--- src/components/HomeComponents/TrendItem.tsx
+++ src/components/HomeComponents/TrendItem.tsx
@@ -1,13 +1,13 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 type Props = {
-  img: String;
+  img: string;
   text: String;
 };
 
 const SearchBar: React.FC<Props> = ({ img, text }) => (
   <div className="flex flex-col justify-center items-center hover:scale-105 hover:cursor-pointer ease-in duration-300 ">
-    <LazyLoadImage alt="dress" src={img} width={150} />
+    <LazyLoadImage alt="dress" src={img} width={150} className="h-[225px]" />
     <span className="mt-2 text-sm ">{text}</span>
   </div>
 );
